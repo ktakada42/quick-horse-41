a04761f2dae2ffcd24d578e463b4fea932c1c1c6
ktakada42 <ktakada950711@gmail.com>
2023-08-27 13:33:36 +0900

    chore: backendディレクトリに移動

---
+++ backend/src/main.ts
@@ -0,0 +1,15 @@
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+
+const router = new Router();
+router.get("/", (context) => {
+  context.response.body = "Welcome to dinosaur API!";
+  console.log("root");
+});
+
+const app = new Application();
+app.use(oakCors()); // Enable CORS for All Routes
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
