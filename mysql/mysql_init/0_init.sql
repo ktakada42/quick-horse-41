b3f4dbf1a0b87351e086b1a2e75c883fec618c40
ktakada42 <ktakada950711@gmail.com>
2023-08-30 19:45:40 +0900

    feat: userテーブルを定義

--- mysql/mysql_init/0_init.sql
+++ mysql/mysql_init/0_init.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE IF NOT EXISTS app;
 
 CREATE TABLE IF NOT EXISTS `user` (
-    `private_user_id` INT AUTO_INCREMENT NOT NULL,
-    `public_user_id` VARCHAR(36) NOT NULL,
-    `user_name` VARCHAR(50) NOT NULL,
-    `id` VARCHAR(50) NOT NULL,
+    `private_user_id` VARCHAR(26) NOT NULL,
+    `public_user_id` VARCHAR(50) NOT NULL,
     `password` VARCHAR(100) NOT NULL,
-    `user_icon_id` VARCHAR(36),
+    `user_name` VARCHAR(50) NOT NULL,
+    `office_id` VARCHAR(26) NOT NULL,
+    `user_icon_id` VARCHAR(26),
+    `reg_date` DATETIME NOT NULL,
+    `isAdmin` BOOLEAN NOT NULL,
     PRIMARY KEY (`private_user_id`)
 );
