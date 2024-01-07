e1c2a54d4a1f9e4a3dd5ca8c30ed3114def5f6fc
yunc <mr.yt510@gmail.com>
2024-01-07 16:19:01 +0900

    feat: review controllerの初期化とエンドポイントを追加

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
 	"app/configs"
 	"app/login"
+	"app/review"
 	"app/user"
 )
+
+// ReviewControllerの初期化
+func initReviewController(db *sql.DB) review.ControllerInterface {
+	reviewRepository := review.NewReviewRepository(db)
+	reviewUseCase := review.NewReviewUseCase(reviewRepository)
+	return review.NewReviewController(reviewUseCase)
+}
 
 func main() {
 	db, err := configs.NewDB()
@@ -22,6 +31,8 @@ func main() {
 	loginUseCase := login.NewLoginUseCase(loginService, loginRepository, userRepository)
 	loginController := login.NewLoginController(loginUseCase)
 
+	reviewController := initReviewController(db)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
@@ -31,6 +42,10 @@ func main() {
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		loginController.Login(w, r)
+	})
+
+	http.HandleFunc("/api/v1/reviews", func(w http.ResponseWriter, r *http.Request) {
+		reviewController.GetReviews(w, r)
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
