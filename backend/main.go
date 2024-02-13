e801ef6fa282b28c59f8dd0f98d81b8775a06d49
yosuke7040 <index_railgu@yahoo.co.jp>
2024-02-14 02:42:51 +0900

    Revert "feat: 本一覧取得API実装"
    This reverts commit 5556aad3824984adecdcd38258594821e24ee651.

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,18 +11,7 @@ import (
 	"app/login/service"
 	"app/login/usecase"
 	ur "app/user/repository"
-
-	bookController "app/book/controller"
-	bookRepository "app/book/repository"
-	bookUseCase "app/book/usecase"
 )
-
-func initBookController(db *sql.DB) bookController.ControllerInterface {
-	bookRepository := bookRepository.NewBookRepository(db)
-	bookUseCase := bookUseCase.NewBookUseCase(bookRepository)
-	bookController := bookController.NewBookController(bookUseCase)
-	return bookController
-}
 
 func main() {
 	db, err := configs.NewDB()
@@ -37,8 +25,6 @@ func main() {
 	loginUseCase := usecase.NewLoginUseCase(loginService, loginRepository, userRepository)
 	loginController := controller.NewLoginController(loginUseCase)
 
-	bookController := initBookController(db)
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
@@ -48,10 +34,6 @@ func main() {
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		loginController.Login(w, r)
-	})
-
-	http.HandleFunc("/books/offices", func(w http.ResponseWriter, r *http.Request) {
-		bookController.GetOfficeBooksController(w, r)
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
